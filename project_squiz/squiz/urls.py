334d56627f9e63f64039523ccc745ac3572f010b
hoodscott <scottdothood@gmail.com>
2016-03-16 15:20:00 +0000

    Adding ajax url

--- project_squiz/squiz/urls.py
+++ project_squiz/squiz/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     url(r'^start/(?P<quiz_id>\w+)$', views.start, name='start'),# start a quiz, create a quiz instance
     
     url(r'^nearby/$', views.nearby, name='nearby'),  #shows pup quizzes and times near to the users location
+    
+    url(r'^get_question/$', views.get_question, name='get_question'),#ajax call to get the current question
 
     url(r'^register/$', views.register, name='register'),
     url(r'^logout/$', views.user_logout, name='logout'),
